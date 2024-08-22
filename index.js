48a8c1b91830ba2f360f7e82f1e9c2e31556d05c
Siddhantwork099 <siddhantwork867@gmail.com>
2024-08-22 23:47:18 +0530

    sid

---
+++ index.js
@@ -0,0 +1,74 @@
+console.log("hello from js file ");
+// ************variable****************************
+// variale is nothig but a container that store the Value 
+var a =44
+var a = 64
+let b = "6" 
+// b = 543
+const c = 24
+//  c = 35
+// b = a
+
+//  ************opretor***************************
+// console.log(3+4);
+// console.log(8-5);
+// console.log(8*2);
+// console.log(8/4);
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a!=b);
+
+// ***************condition*************** 
+// var age = 13
+//  if(age>=18){ 
+//     console.log("you can drive");
+    
+//  }else{ 
+//     console.log("you cant drive ");
+    
+//  }
+
+// var math = 33
+// var chemisry = 54
+// var phy = 98
+// let per =  (math + chemisry + phy ) /3
+// if(per >=33 && math >=33 && phy>=33 && chemisry>=33){ 
+//     console.log(' yes you are passed the exam your persentage is :- ', per );
+    
+// }else{ 
+//     console.log("you r fail in the exam ");
+    
+// }
+
+// ***************loop*************************
+// console.log(2);
+// console.log(2*2);
+// console.log(2*3);
+// console.log(2*4);
+// console.log(2*5);
+// for( var i =1 ; i<=10;i++){ 
+//     console.log(2*i);
+    
+// }
+
+
+// while (aam>0)
+// { 
+//     console.log("you can eat mango " , aam)
+//     aam = aam -1
+    
+// }
+// 5!  = 5*4*3*2*1 =  120
+var num = 5 
+let fec = 1
+for(  i=1; i<=num; i++){ 
+    
+    fec = fec*i
+    
+    
+}
+console.log(fec);
